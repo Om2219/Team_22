2a3717a1e91bab98f97eb7762d4aac31b6d3e5a7
gurmultani500-alt <gurmultani500@gmail.com>
2025-11-24 21:56:58 +0000

    version 1
    version 1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,11 +22,16 @@
         <div class= "top">
             <a href="/home"><button class = "logoButton"><img src="{{ Vite::asset('resources/images/logo_updated.png') }}" class = "logo"></button></a>
             <input type="text" name="search" placeholder="What are you looking for?">
+<<<<<<< Updated upstream
             <a href="/account"><button class ="headbut"><img src="{{ Vite::asset('resources/images/account.png') }}" class = "bob"><br>Account</button></a>
              <a href="/login"><button class ="headbut"><img src="{{ Vite::asset('resources/images/account.png') }}" class = "bob"><br>login</button></a>
              <a href="/sign-in"><button class ="headbut"><img src="{{ Vite::asset('resources/images/account.png') }}" class = "bob"><br>sign-in</button></a>
 
              <a href="/basket"><button class ="headbut"><img src="{{ Vite::asset('resources/images/basket.png') }}" class = "bob"><br>Basket</button></a>
+=======
+            <a href="/account"><button class ="headbut"><img src="{{ Vite::asset('resources/images/account.png') }}" class = "bob"><br>Accounts</button></a>
+            <a href="/basket"><button class ="headbut"><img src="{{ Vite::asset('resources/images/basket.png') }}" class = "bob"><br>Basket</button></a>
+>>>>>>> Stashed changes
             </div>
           {{-- bottom of nav bar and contains our 5 catagoires with links, and if you hover over it gives you more specific options--}}    
         <div class= "bottom">
