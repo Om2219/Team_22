cef9dff80f82bb6a93ad8e79544270d144ac13e6
SionGunton <SionGunton220506@gmail.com>
2025-11-25 17:57:26 +0000

    Database functionality

--- database/migrations/2025_11_25_165552_create_product_images_table.php
+++ database/migrations/2025_11_25_165552_create_product_images_table.php
@@ -6,19 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            //$table->timestamp('created_when')->nullable();
+            $table->string('product_image')->nullable();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('product_images');
     }
 };
