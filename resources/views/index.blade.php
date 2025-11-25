06c480fe207fdb54385ef0f557dd4792be1c26fd
gurmultani500-alt <gurmultani500@gmail.com>
2025-11-25 20:45:08 +0000

    update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,32 @@
 <x-layout>
  
-    <div >
-    <p> TEST </p>
-    </div>
+    <div class="slideshow-container">
+
+<!-- full -width images-->
+<div class="mySlides fade">
+    <div class="numbertext"> 1/3</div>
+    <img src="img1.png" style="width:100%">
+<div class="text"> Caption Text</div>
+
+</div>
+
+<div class="mySlides fade">
+    <div class="numbertext"> 2/3</div>
+    <img src="img2.png" style="width:100%">
+<div class="text"> Caption Two</div>
+
+</div>
+
+<div class="mySlides fade">
+    <div class="numbertext"> 3/3</div>
+    <img src="img1.png" style="width:100%">
+<div class="text"> Caption Three</div>
+
+</div>
 
    
+
+
+
+
 </x-layout>
