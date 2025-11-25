787abd49df83924769a50adc59a8cdbe6db37f56
Haidencz <haider.writing@gmail.com>
2025-11-25 19:08:01 +0000

    LOGIN/SIGNUP set up

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <p><a href="/login">Login</a> | <a href="/register">Register</a></p>
+    </body>
+</html>
+
---
+++ storage/framework/views/8071c3f08a960359d492a39ef6c75dbc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <p><a href="/login">Login</a> | <a href="/register">Register</a></p>
+    </body>
+</html>
+
+<?php /**PATH C:\Users\haide\Downloads\GitHub\Team_22\resources\views/welcome.blade.php ENDPATH**/ ?>
