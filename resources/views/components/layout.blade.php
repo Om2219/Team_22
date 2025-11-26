80dd280dc241c3c6b0525ce5d6a8fbc435cf409e
gurmultani500-alt <gurmultani500@gmail.com>
2025-11-26 12:09:19 +0000

    updated layout and finished login page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,6 +23,9 @@
             <a href="/home"><button class = "logoButton"><img src="{{ Vite::asset('public/images/logo_updated.png') }}" class = "logo"></button></a>
             <input type="text" name="search" placeholder="What are you looking for?">
             <a href="/account"><button class ="headbut"><img src="{{ Vite::asset('public/images/account.png') }}" class = "bob"><br>Account</button></a>
+            <a href="/login"><button class ="headbut"><img src="{{ Vite::asset('public/images/account.png') }}" class = "bob"><br>Login</button></a>
+            <a href="/register"><button class ="headbut"><img src="{{ Vite::asset('public/images/account.png') }}" class = "bob"><br>Register</button></a>
+
             <a href="/basket"><button class ="headbut"><img src="{{ Vite::asset('public/images/basket.png') }}" class = "bob"><br>Basket</button></a>
             </div>
           {{-- bottom of nav bar and contains our 5 catagoires with links, and if you hover over it gives you more specific options--}}    
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,116 @@
 <x-layout>
- 
-    <div >
-    <p> place holder </p>
-    </div>
+ <div class="loginPage">
+    <form action="action_page.php" method="post" class="loginForm">
+                <div class="imgcontainer">
+            <img src="{{ asset('images/img_avatar2.png') }}" alt= "Avatar" class ="avatar">
+</div>
 
+<div class= "login">
+    <label for= "uname"><b>Username</b></label>
+    <input id="uname" type="text" placeholder= "Enter Username" name="uname" required>
+
+<label for= "psw"><b>Password</b></label>
+    <input id="psw" type="password" placeholder= "Enter Password" psw="uname" required>
    
+<button type ="submit" class="mainLogin"> Login</button>
+<label>
+    <input type = "checkbox" checked name="remember"> Remember Me
+</label>
+</div>
+
+
+<div class="login extra"> 
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forget <a href="#"> password?</a></span>
+</div>
+</form>
+
+
+
+
+<style>
+/* border form */
+.loginPage .loginForm{
+    width: 380px;
+    margin: 40px auto;
+    background: white;
+    border: 3px solid #7a4900;
+    border-radius: 8px;
+}
+
+/*inputs */
+.loginPage input[type=text], 
+.loginPage input[type=password]{
+    width:100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    display: inline-block;
+    box-sizing: border-box;
+}
+
+/*style for all buttons */
+.loginPage .loginForm button{
+    cursor:pointer;
+}
+
+.loginPage .mainLogin{
+    background-color: #bdab53;
+    color: black;
+    padding: 14px 20px;
+    margin; 8px 0;
+    border: none;
+    width: 100%;
+    font-weight: 600;
+    margin-top: 8px;
+}
+/*effects for buttons */
+.loginForm button:hover{
+    opacity: 0.8;
+}
+
+/**extra style for the cancel button */
+.loginPage .cancelbtn{
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+/**avatar image */
+.loginPage .imgcontainer{
+    text-align: center;
+    margin-top: 20px;
+}
+.loginPage img.avatar{
+    width: 40%;
+    border-radius: 50%;
+}
+
+/**padding to containers */
+.loginPage .login{
+    padding: 16px;
+}
+ .loginPage .login.extra{
+    background-color:#f1f1f1;
+}
+
+/** forgot password */
+span.psw{
+    float: right;
+    padding-top: 16px;
+}
+
+/**change style from span and cancel button on extra small */
+@media screen and (max-width: 300px){
+    span.psw{
+        display: block;
+        float: none;
+    }
+     .loginPage .cancelbtn{
+        width:100%;
+    }
+}
+
+</style>    
+
 </x-layout>
