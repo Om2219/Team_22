c80dcb61725d3b294ff4810dde9c76846140dbbe
Ashpreet Chahal <ashpreetchahal1@gmail.com>
2025-11-29 17:17:29 +0000

    Update ContactFormController.php
    Basic Form for testing

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class ContactFormController extends Controller {
     public function submit(Request $request) {
-        
-    }
+        $request->validate([ 'name' => 'required',
+        'email' => 'required', ]);
 
+        return redirect('/contact')->with('success', 'done - worked');
+    }
 }
