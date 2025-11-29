a0b96b86e756704f97c4eb988833963da025fb59
Kyle <150052231+technooooooo@users.noreply.github.com>
2025-11-29 22:43:03 +0000

    FAQ page updates
    Organised CSS code, organised faq page a little better

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,15 +1,7 @@
 <x-layout>
 
     <h1>My Basket</h1>
-
-<div class = "basket-master">
-    <button class="checkoutButton"><a href="/checkout">Checkout</a></button>
-</div>
-
-<style>
-    /* PUT BASKET CONTENTS IN A RECTANGULAR CONTAINER LATER */
-</style>
-</x-layout>
+    
     <div class="MasterBasketBox">
 
         @if($basket->isEmpty())
