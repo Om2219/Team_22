af665c194071d041abceb44c16ddf33c5ac6b4aa
Ashpreet Chahal <ashpreetchahal1@gmail.com>
2025-12-04 19:51:28 +0000

    Update ContactFormController.php
    adding comments

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller {
+    // Handles contact form submission
+    // Returns to the contact page with a success message
     public function submit(Request $request) {
         $request->validate([
+            // Below are the fields that need to be filled in the contact form
             'name' => 'required|string',
             'email' => 'required|email',
             'subject' => 'required|string',
             'message' => 'required|string'
         ]);
-
+        // success message
         return redirect('/contact')->with('success', 'Thank you for reaching out. Your message is with our team and you will recieve a response shortly.');
     }
 }
+
+// ready for front-end integration
