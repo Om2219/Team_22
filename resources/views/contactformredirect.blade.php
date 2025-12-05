f3f2bf28b43a673557806e153a2e579e538baf9b
Ashpreet Chahal <ashpreetchahal1@gmail.com>
2025-12-05 10:44:03 +0000

    Create contactformredirect.blade.php
    created a new page so that theres redirection after success

---
+++ resources/views/contactformredirect.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h1>Thank You for Your Message!</h1>
+
+    <p> We appreciate you taking the time to contact us.</p>
+    <p> Our team will review your message and get back to you as soon as possible.</p>
+
+    <br><button class ="save-btn"><a href="/contactform">Return to Form</a></button>
+</x-layout>
