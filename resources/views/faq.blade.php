d298d1c956a14d231b28dec35715af7a04ca68e2
Kyle <150052231+technooooooo@users.noreply.github.com>
2026-02-07 18:40:57 +0000

    FAQ page finishing touches
    Button changes, image changes, code adjustments, etc.

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -7,96 +7,64 @@
     <div class="faq-master">
         <br>
         <button class="questions" onclick="tog(this)">Can I track my order?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- ONLY CHANGE THIS IF YOU'RE WILLING TO GET THE 
-                ARROWS TO DISAPPEAR AND REAPPEAR UPON BUTTON CLICK --}}
-                {{-- {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>You can track your order by going into the <a class="faq-hyperlinks" href="/account">account</a> page and clicking on 'my orders'.</p></div>
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">What is the refund policy?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>All customers get a 14-day return policy.</p></div>
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">How do I return an item?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>Send it back in its original packaging, and process a return ticket in the <a class="faq-hyperlinks" href="/account">account</a> page.</p></div>
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">How do I reset my password?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>In the <a class="faq-hyperlinks" href="/account">account</a> page, you can reset your account details.</p></div>
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">What do I do if it doesn't let me log in?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>Contact customer support via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page.</p></div>
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">What should I do if my order doesn't show up?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>Customer support can be contacted via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page, they should be able to resolve these issues.</p></div> 
-        <br><hr><br>
+        <br>
 
         <button class="questions" onclick="tog(this)">Do you offer international delivery?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>We currently only do deliveries in the United Kingdom, and Republic of Ireland.</p></div>
-        <br><hr><br>
+        <br>
        
         <button class="questions" onclick="tog(this)">What payment methods do you offer?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>At Roots, we offer card payment (Credit and Debit) and PayPal.</p></div>
-        <br><hr><br>
+        <br>
         
         <button class="questions" onclick="tog(this)">How long are orders expected to take?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>Orders are expected to take 1-2 weeks.</p></div>
-        <br><hr><br>
+        <br>
         
         <button class="questions" onclick="tog(this)">When should my refund be processed?
-            <div class="faq_arrows">
-                <img src="images/dropdown-arrow_faq.png" height="20" width="20">
-                {{-- <img src="images/up-arrow_faq.png" height="20" width="20"> --}}
-            </div>
+            <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
         <div class="answer"><p>Refunds usually take around 5-10 business days.</p></div>
-        <br><hr><br>
+        <br>
     </div>
 
 </x-layout>
