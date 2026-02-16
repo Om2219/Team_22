d23e89bd03b0d71e3765e000022eaa908873ae15
gurmultani500-alt <gurmultani500@gmail.com>
2026-02-16 14:06:02 +0000

    test
    test

--- storage/framework/views/39a3b4013d516813610da122e3ac70b9.php
+++ storage/framework/views/39a3b4013d516813610da122e3ac70b9.php
@@ -24,7 +24,10 @@
         <div class= "top">
             <a href="/home"><button class = "logoButton"><img src="<?php echo e(Vite::asset('public/images/logo_updated.png')); ?>" class = "logo"></button></a>
             
+<<<<<<< Updated upstream
             <!-- <form action="<?php echo e(route('products.search')); ?>" method="GET">  what it used to be-->
+=======
+>>>>>>> Stashed changes
             <form action="<?php echo e(route('search')); ?>" method="GET">
               <input type="text" name="search" placeholder="What are you looking for?" class="headbut">
               <button type="submit" class = "bob">🐒</button>
