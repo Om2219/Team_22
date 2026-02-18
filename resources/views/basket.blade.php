2b97c52d59e43f887c8950a560cbcad4ee2f813f
Haidencz <haider.writing@gmail.com>
2026-02-18 20:18:36 +0000

    VOUCHER FUNCTIONALITY COMPLETE
    Voucher in BASKET
    updates TOTAL
    has redundancies if wrong code used
    REMOVE VOUCHER BUTTON
    SAVE10, FIVEOFF and MONKEY are the codes

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -4,7 +4,7 @@
 
     <div class="MasterBasketBox">
 
-        @if($basket->isEmpty())
+        @if($basket->isEmpty()) 
             <p>The basket is currently empty.</p>
         @else
         <div class="basket-container">
@@ -42,8 +42,36 @@
             </div>
 
             <div class="basket_total">
-                <p>Grand total: £{{number_format($totalPrice, 2)}}</p>
+                <p>Subtotal: £{{ number_format($totalPrice, 2) }}</p>
+                @if ($voucher)
+                <P style="color: green;"><strong>Voucher ({{  $voucher->code }}):</strong>-£{{  number_format($discount, 2) }}</P>
+                @if($voucher->min_spend && $totalPrice < $voucher->min_spend)
+                <p style="color: red;"> Spend £{{  number_format($voucher->min_spend - $totalPrice, 2) }} more to use this voucher</p>
+                @endif
+                @endif
+                <hr>
+                <p style="font-size: 1.2rem;"> <strong>Total:</strong> £{{  number_format($finalTotal, 2) }}</p>
             </div>
+            <h3>Voucher</h3>
+            @if (session('voucher_error'))
+            <p style="color:red;">{{  session('voucher_error') }}</p>
+            @endif
+            @if (session('voucher_success'))
+            <p style="color:green;">{{  session('voucher_success') }}</p>
+            @endif
+            @if (!session()->has('voucher'))
+                <form method="POST" action="{{ route('voucher.apply') }}">
+                    @csrf
+                    <input type="text" name="code" placeholder="Enter voucher code" required>
+                    <button type="submit" class="save-btn">Apply</button>
+                </form>
+            @else
+            <p> Voucher Applied: <strong>{{  session('voucher.code') }}</strong></p>
+            <form method="POST" action="{{  route('voucher.remove') }}">
+                @csrf
+                <button type="submit" class="save-btn">Remove voucher</button>
+            </form>
+            @endif   
 
             <div class = "basket-master"> <!--keeping this outside the container-->
                 <a href="/checkout" class="save-btn">Checkout</a>
