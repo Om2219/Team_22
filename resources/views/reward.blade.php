2482150e629dfa8b221823480899a4897551672e
Haidencz <haider.writing@gmail.com>
2026-02-26 15:50:01 +0000

    VOUCHER REWARDS BUTTON
    + BUTTON TO REWARDS PAGE

--- resources/views/reward.blade.php
+++ resources/views/reward.blade.php
@@ -4,4 +4,7 @@
 
     <button class = "headbut"><a href="/slots">feeling lucky?</a></button>
 
+    <a href="{{ route('points.vouchers') }}" class="save-btn">
+    Buy vouchers with points
+</a>
 </x-layout>
