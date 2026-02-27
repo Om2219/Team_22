22b8d77347d999da08c7303d6ff75d1f03cee47d
Kyle <150052231+technooooooo@users.noreply.github.com>
2026-02-27 22:27:46 +0000

    Contact form and small changes
    CSS for contact form as well as some small changes to FAQ page.

--- resources/views/contactform.blade.php
+++ resources/views/contactform.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-    <h1>Contact Us</h1>
 
     <div class="contactUsPage-master">
+        <h1>Contact Us</h1><br>
         <form method="POST" action="{{route('contactform.submit')}}">
             @csrf
             <p>Please enter your details below to send us an email.</p>
@@ -10,13 +10,13 @@
             <p><label for="name">Name</label></p> <!--each label is in a p element, so labels appear above their respective fields (makes specifically the textarea field look better, and has the others match its styling)-->
             <input id="name" type="text" placeholder="Enter First and Last Names" name="name" required>
             
-            <p><label for="email">Email</label></p>
+            <p><label for="email"><br>Email</label></p>
             <input id="email" type="email" placeholder="Enter Email" name="email" required>
             
-            <p><label for="subject">Subject</label></p>
+            <p><label for="subject"><br>Subject</label></p>
             <input id="subject" type="text" placeholder="Enter Message Subject" name="subject" required>
             
-            <p><label for="message">Message</label></p>
+            <p><label for="message"><br>Message</label></p>
             <!--textarea for the purpose of writing an email on websites, so text wraps down-->
             <textarea id="message" rows="20" cols="50" type="text" placeholder="Enter Message" name="message" required></textarea> <!--rows 20 makes the field a suitable height, cols 50 makes the field a suitable width-->
             <hr>
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -9,61 +9,61 @@
         <button class="questions" onclick="tog(this)">Can I track my order?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>You can track your order by going into the <a class="faq-hyperlinks" href="/account">account</a> page and clicking on 'my orders'.</p></div>
+        <div class="answer"><p><br>You can track your order by going into the <a class="faq-hyperlinks" href="/account">account</a> page and clicking on 'my orders'.</p></div>
         <br>
 
         <button class="questions" onclick="tog(this)">What is the refund policy?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>All customers get a 14-day return policy.</p></div>
+        <div class="answer"><p><br>All customers get a 14-day return policy.</p></div>
         <br>
 
         <button class="questions" onclick="tog(this)">How do I return an item?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>Send it back in its original packaging, and process a return ticket in the <a class="faq-hyperlinks" href="/account">account</a> page.</p></div>
+        <div class="answer"><p><br>Send it back in its original packaging, and process a return ticket in the <a class="faq-hyperlinks" href="/account">account</a> page.</p></div>
         <br>
 
         <button class="questions" onclick="tog(this)">How do I reset my password?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>In the <a class="faq-hyperlinks" href="/account">account</a> page, you can reset your account details.</p></div>
+        <div class="answer"><p><br>In the <a class="faq-hyperlinks" href="/account">account</a> page, you can reset your account details.</p></div>
         <br>
 
         <button class="questions" onclick="tog(this)">What do I do if it doesn't let me log in?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>Contact customer support via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page.</p></div>
+        <div class="answer"><p><br>Contact customer support via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page.</p></div>
         <br>
 
         <button class="questions" onclick="tog(this)">What should I do if my order doesn't show up?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>Customer support can be contacted via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page, they should be able to resolve these issues.</p></div> 
+        <div class="answer"><p><br>Customer support can be contacted via the <a class="faq-hyperlinks" href="/contactform">contact us</a> page, they should be able to resolve these issues.</p></div> 
         <br>
 
         <button class="questions" onclick="tog(this)">Do you offer international delivery?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>We currently only do deliveries in the United Kingdom, and Republic of Ireland.</p></div>
+        <div class="answer"><p><br>We currently only do deliveries in the United Kingdom, and Republic of Ireland.</p></div>
         <br>
        
         <button class="questions" onclick="tog(this)">What payment methods do you offer?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>At Roots, we offer card payment (Credit and Debit) and PayPal.</p></div>
+        <div class="answer"><p><br>At Roots, we offer card payment (Credit and Debit) and PayPal.</p></div>
         <br>
         
         <button class="questions" onclick="tog(this)">How long are orders expected to take?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>Orders are expected to take 1-2 weeks.</p></div>
+        <div class="answer"><p><br>Orders are expected to take 1-2 weeks.</p></div>
         <br>
         
         <button class="questions" onclick="tog(this)">When should my refund be processed?
             <img src="images/dropdown-arrow_faq.png" height="20" width="20">
         </button>
-        <div class="answer"><p>Refunds usually take around 5-10 business days.</p></div>
+        <div class="answer"><p><br>Refunds usually take around 5-10 business days.</p></div>
         <br>
     </div>
 
