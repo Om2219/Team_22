26b8ba623ee827a982e251a9ff9e44ed51d0154b
Om2219 <om4873460@gmail.com>
2026-03-14 10:03:02 +0000

    search fixed
    search now shows all products begining wiht letter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -262,7 +262,7 @@ class ProductController extends Controller
         }
 
         // 1. Join reviews and calculate average rating from your fresh seeders
-        $query = Product::where('name', 'LIKE', "%{$search}%")
+        $query = Product::where('name', 'LIKE', "{$search}%")
                         ->withAvg('reviews', 'rating');
 
         // 2. Sorting logic
