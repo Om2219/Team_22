2c25fc029f42426bb35760da9f99f793dd125bf3
Om2219 <om4873460@gmail.com>
2026-03-21 20:07:30 +0000

    create products blade css

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,18 +1,21 @@
 <x-layout>
-    <h1> Add product</h1>
+    <div class="container my-4">
+        <h1 class="mb-0 fw-bold"> Add product</h1><br>
 
-    <form action="{{  route('products.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label>Product Name</label><br>
-            <input type="text" name="name" value="{{ old('name') }}" required>
-        </div>
-        <br>
-        <div>
+        <form action="{{  route('products.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
             <div class="mb-3">
-                <label for="category_id" class="form-label">Category</label>
+                <label class="form-label fw-bold">Product Name</label><br>
+                <input class="form-control" type="text" name="name" value="{{ old('name') }}" required>
+            </div>
+            <br>
 
-                <select name="category_id" id="category_id" class="form-control" required>
+
+            <div class="mb-3">
+                <label class="form-label fw-bold" for="category_id" class="form-label">Category</label>
+
+                <select class="form-select" name="category_id" id="category_id" class="form-control" required>
                     <option value="">-- Select a category --</option>
 
                     @foreach ($categories as $category)
@@ -23,42 +26,46 @@
                 </select>
             </div>
 
-        </div>
-        <br>
-        <div>
-            <label>Description</label><br>
-            <textarea name="product_description">{{  old('product_description') }}</textarea>
-        </div>
+            <br>
 
-        <br>
-
-        <div>
-            <label>Price</label><br>
-            <input type="number" step="0.01" name="price" value="{{ old('price') }}" required>
-        </div>
-
-        <br>
-        <div class="" mb-3>
-            <label>Stock</label>
-            <input type="number" name="stock" id="stock" min="0" value="{{  old('stock', 0) }}" required>
-        </div>
-        <br>
-        <div class="mb-3">
-            <label>Low Stock Threshold</label>
-            <input type="number" name="low_stock" id="low_stock" min="0" value="{{  old('low_stock', 29) }}" required>
-        </div>
-
-        <div>
-            <label>Product Images</label><br>
-            <input type="file" name="product_image" accept="image/*">
-        </div>
-        <br>
-        <button type="submit">Add Product</button>
-
-        @if ($errors->any())
-            <div style="color: red; margin-top: 10px;">
-                {{  $errors->first() }}
+            <div class="mb-3">
+                <label class="form-label fw-bold">Description</label><br>
+                <textarea class="form-control" name="product_description">{{  old('product_description') }}</textarea>
             </div>
-        @endif
-    </form>
+            <br>
+
+            <div class="row">
+                <div class="col mb-3">
+                    <label class="form-label fw-bold">Price</label><br>
+                    <input class="form-control" type="number" step="0.01" name="price" value="{{ old('price') }}" required>
+                </div>
+                <br>
+
+                <div class="col mb-3">
+                    <label class="form-label fw-bold">Stock</label><br>
+                    <input class="form-control" type="number" name="stock" id="stock" min="0" value="{{  old('stock', 0) }}" required>
+                </div>
+                <br>
+
+                <div class="col mb-3">
+                    <label class="form-label fw-bold">Low Stock Threshold</label><br>
+                    <input class="form-control" type="number" name="low_stock" id="low_stock" min="0" value="{{  old('low_stock', 29) }}" required>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label fw-bold">Product Images</label><br>
+                <input class="form-control" type="file" name="product_image" accept="image/*">
+            </div>
+            <br>
+
+            <button class="btn btn-outline-secondary btn-sm" type="submit">Add Product</button>
+
+            @if ($errors->any())
+                <div style="color: red; margin-top: 10px;">
+                    {{  $errors->first() }}
+                </div>
+            @endif
+        </form>
+    </div>
 </x-layout>
